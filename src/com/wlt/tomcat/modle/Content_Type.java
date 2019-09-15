ee69a3ff9cb8194ad16007f1c8d0becf95d96d6c
LitengWang <1242251907@qq.com>
2019-09-15 21:53:16 +0800

    把相应文件类型提前到modle类中

---
+++ src/com/wlt/tomcat/modle/Content_Type.java
@@ -0,0 +1,17 @@
+package com.wlt.tomcat.modle;
+
+/**
+ * @author 王立腾
+ * @date 2019-09-15 21:04
+ */
+public class Content_Type {
+
+    //相应体的数据类型
+    public static final String TXT = "text/html;charset=UTF-8";
+    public static final String PNG = "image/png";
+    public static final String JPG = "image/jpeg";
+    public static final String GIF = "image/gif";
+    public static final String GSON = "";
+    public static final String STREAM = "";
+
+}
